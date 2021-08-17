c9c254324042b0944be46e35576451075d224c3a
Dian Aditya <dianw.aditya@gmail.com>
2021-08-18 00:20:56 +0700

    Added test config

--- metadata/src/main/java/org/enkrip/frappe/metadata/Application.java
+++ metadata/src/main/java/org/enkrip/frappe/metadata/Application.java
@@ -3,9 +3,12 @@ package org.enkrip.frappe.metadata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.scheduler.Schedulers;
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        Schedulers.enableMetrics();
         SpringApplication.run(Application.class, args);
     }
 }
--- metadata/src/main/java/org/enkrip/frappe/metadata/core/cassandra/CassandraConfig.java
+++ metadata/src/main/java/org/enkrip/frappe/metadata/core/cassandra/CassandraConfig.java
@@ -1,6 +1,7 @@
 package org.enkrip.frappe.metadata.core.cassandra;
 
 import org.enkrip.frappe.metadata.Application;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -11,6 +12,7 @@ import org.springframework.data.cassandra.repository.config.EnableReactiveCassan
 
 @Configuration
 @EnableReactiveCassandraRepositories(basePackageClasses = Application.class)
+@ConditionalOnBean(SessionFactory.class)
 public class CassandraConfig {
     @Bean
     public SessionFactoryInitializer sessionFactoryInitializer(SessionFactory sessionFactory) {
